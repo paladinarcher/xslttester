b01343970f98c28829528f8cc72f2623de22ed91
Jay Ward <jay@paladinarcher.com>
2018-11-23 13:55:52 -0700

    updating icon to not be lame

--- XSLTest/Main.Designer.cs
+++ XSLTest/Main.Designer.cs
@@ -323,6 +323,7 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Main";
             this.Text = "XSLT Debugger";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
